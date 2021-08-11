57b920a931bf416ecdb96629070b66c954e8acbe
jones-trae <jones.trae@gmail.com>
2021-08-10 17:48:28 -0700

    initial commit of ind files

---
+++ js/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+console.log('test');
+
+let questionOne = prompt('Do you feel like you REALLY know Valton?');
+console.log(questionOne);
+
+if (questionOne === 'yes') {
+  alert('Really? Well you can always know more.')
+} else if (questionOne === 'no') {
+  alert('Does anyone REALLY truly know the OTHER')
+} else {
+  alert('Fascinating.')
+}
