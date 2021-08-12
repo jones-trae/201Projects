2eee083b350397a252cace57323b8ac4fd2da4f2
jones-trae <jones.trae@gmail.com>
2021-08-11 20:13:35 -0700

    edits

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ About Me
 
 LAB - 02
 About Me
-A short 5 question website about me utilizing HTML and JS
+A short 5 question website about me utilizing HTML and JS, followed by a Top ten favorite things I like to eat.s
 
 Author: Valton Jones/Group Name
--- js/app.js
+++ js/app.js
@@ -2,6 +2,10 @@
 
 console.log('test');
 
+
+
+
+
 let introA = prompt('Before you get to know Valton, what is your name?'); 
  //console.log(introA);
  alert('Welcome to the quiz,' + introA + ' ,let us begin.')
@@ -46,6 +50,18 @@ if (questionThree === 'yes' || questionThree === 'y') {
   alert('Fascinating.')
 }
 
+let questionFourA = prompt('Does he hike enough?'); 
+ //console.log(questionFourA);
+let questionFour = questionFourA.toLowerCase();
+
+if (questionFour === 'yes' || questionFour === 'y') {
+  alert('That is incorrect!!!!')
+} else if (questionFour === 'no' || questionFour ===  'n') {
+  alert('Unfortunately, he does not.')
+} else {
+  alert('Fascinating.')
+}
+
 let questionFiveA = prompt('Does he have a mustache in his picture?'); 
  //console.log(questionFiveA);
 let questionFive = questionFiveA.toLowerCase();
@@ -57,6 +73,54 @@ if (questionFive === 'yes' || questionFive === 'y') {
 } else {
   alert('Fascinating.')
 }
+
+let numGuess;
+let goodGuess = 4;
+let attempts = 0;
+let intNumGuess;
+
+while (intNumGuess !== goodGuess && attempts < 4) {
+  numGuess = prompt('Try to pick a number 1 to 10. You have ' + (4-attempts) + ' left.');
+  intNumGuess = parseInt(numGuess)
+  attempts++
+  if (intNumGuess !== goodGuess) {
+    if (intNumGuess > goodGuess) {
+      alert('Too high! Try again.')
+    }else {
+      alert('Too low! Try again')
+    }
+  }
+}
+if (intNumGuess === goodGuess) {
+  alert('Noice!');
+} else if (intNumGuess !== goodGuess) {
+  alert('Better luck next time...it was ' + goodGuess + '!')
+}
+
+
+let attempts = 0;
+let favHike = false;
+let bestHike = ['Mount Jupiter', 'Gold Mountain', 'Green Mountain', 'Tubal Cain Mine', 'Upper Snohomish', 'Bridal Veil Falls', 'Hurricane Ridge', 'The Hoh Rainforest', 'Table Mountain', 'The Brothers'];
+
+while (attempts < 5 && favHike === false) {
+  let hike = prompt('Can you remember one of my favorite hikes?');
+  for (let i = 0; i < bestHike.length; i++){
+    let currentHike = bestHike[i];
+    if(hike === currentHike){
+      alert('Oh my god I LOVE that hike!');
+      favHike = true;
+    }
+  }
+  attempts++;
+  if(favHike === false){
+    alert('Never been there. Lets go?');
+  }
+}
+
+
+
+
+
 
 let exitMSG = alert('Thanks for playing,' + introA + ' !')
 
